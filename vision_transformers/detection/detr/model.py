6e125716acf11c03e267e9dd76cb9b88ee1982e4
LamNguyen2608 <phuclam.work@gmail.com>
2024-07-17 15:32:21 +0800

    model.py add 2 output linear

--- vision_transformers/detection/detr/model.py
+++ vision_transformers/detection/detr/model.py
@@ -2,17 +2,24 @@ import torch
 import torch.nn as nn
 
 class DETRModel(nn.Module):
-    def __init__(self, num_classes=1, model='detr_resnet50'):
+    def __init__(self, num_classes=4, num_birads=4, model='detr_resnet50'):
         super(DETRModel, self).__init__()
         self.num_classes = num_classes
+        self.num_birads = num_birads
+        
         self.model = torch.hub.load(
             'facebookresearch/detr', 
             model, 
             pretrained=True,
         )
-        self.out = nn.Linear(
-            in_features=self.model.class_embed.out_features, 
-            out_features=num_classes
+        self.out1 = nn.Linear(
+            in_features1=self.model.class_embed.out_features1, 
+            out_features1=num_classes
+        )
+
+        self.out2 = nn.Linear(
+            in_features2=self.model.class_embed.out_features2, 
+            out_features2=num_birads
         )
         
     def forward(self, images):
@@ -29,5 +36,6 @@ class DETRModel(nn.Module):
             'embed': [p for n,p in self.model.named_parameters()
                                  if (('class_embed' in n) or ('bbox_embed' in n) or ('query_embed' in n)) 
                            and p.requires_grad],
-            'final': self.out.parameters()
+            'final1': self.out1.parameters(),
+            'final2': self.out2.parameters()
         }
